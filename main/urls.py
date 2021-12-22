06b16295ca2ed24e24e9f037d01816016ae71545
User <erickdelenia08>
2021-12-22 08:14:24 +0700

    coba 4

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,17 @@
-from django.urls import path
-
+# from django.urls import path
+#
+# from . import views
+#
+# app_name = 'main'
+#
+# urlpatterns = [
+#     path('', views.home, name='home'),
+# ]
+from django.contrib import admin
+from django.conf.urls import url,include
 from . import views
-
-app_name = 'main'
-
-urlpatterns = [
-    path('', views.home, name='home'),
+urlpatterns=[
+     url(r'^admin/',admin.site.urls),
+     url(r'^register/',include('blog.urls')),
+     url(r'^$',views.home),
 ]
